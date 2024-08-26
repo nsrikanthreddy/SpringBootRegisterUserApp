f42905d8f14199796b69d194db073faad41ec31a
nsrikanthreddy <nallaballesrikanthreddy@gmail.com>
2024-08-26 15:14:42 +0530

    Update User.java

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO)
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private int id;
 	@Column(name = "name")
 	private String name;
