7d47f6ba047fc6b34d68b23b60a8b763795f5c03
nsrikanthreddy <nallaballesrikanthreddy@gmail.com>
2024-08-26 16:31:18 +0530

    Update UserRestController.java

--- src/main/java/com/example/demo/UserRestController.java
+++ src/main/java/com/example/demo/UserRestController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 //@CrossOrigin(origins = "http://localhost:9092" /*{"*","http://app:8080", "http://localhost:3000"}*/, allowedHeaders="*", exposedHeaders = {"Access-Control-Allow-Origin"})
 @RestController
