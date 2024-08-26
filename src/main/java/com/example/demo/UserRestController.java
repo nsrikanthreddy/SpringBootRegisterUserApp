f8538fb8063d6cda0b09d537f60b795f0f436c54
nsrikanthreddy <nallaballesrikanthreddy@gmail.com>
2024-08-26 15:16:28 +0530

    Update UserRestController.java

--- src/main/java/com/example/demo/UserRestController.java
+++ src/main/java/com/example/demo/UserRestController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*" /*{"http://app:8080", "http://localhost:3000"}*/, allowedHeaders="*", exposedHeaders = {"Access-Control-Allow-Origin"})
+@CrossOrigin(origins = "http://localhost:9092" /*{"*","http://app:8080", "http://localhost:3000"}*/, allowedHeaders="*", exposedHeaders = {"Access-Control-Allow-Origin"})
 @RestController
 @RequestMapping(value= "/test")
 public class UserRestController extends JdbcDaoSupport {
